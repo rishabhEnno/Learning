8bcd22b3ff01e5283c48ed32f58831b16e84fc42
Rishabh <rishabh.singhal@ennoventure.com>
2022-10-25 11:29:01 +0530

    test raise exception pytest

--- tdd.py
+++ tdd.py
@@ -12,6 +12,13 @@ def test_answer():
 class TestSomething:
     def func(self, x):
         return x + 1
+    
+    def func_zero_division_error(self):
+        return 1/0
 
     def test_answer(self):
         assert self.func(3) == 4
+
+    def test_answer_exception(self):
+        with pytest.raises(ZeroDivisionError):
+            self.func_excep()
